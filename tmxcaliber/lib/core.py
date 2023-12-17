e0b5b7ddcc39545650930a242e3a0dd4c42e7d15
Jonathan Rault <jon@trustoncloud.com>
2023-12-17 10:28:09 +0900

    Add core.py to tmxcaliber library.

---
+++ tmxcaliber/lib/core.py
@@ -0,0 +1,6 @@
+# Core library functions for tmxcaliber
+
+def filter_down(data: dict, severity: str = "", events: list = [], permissions: list = [], feature_classes: list = [], ids: list = []) -> dict:
+    # ... existing logic for filter_down ...
+    # This function should now accept parameters directly and not rely on the Namespace object.
+    # Return the filtered data as a dictionary.
