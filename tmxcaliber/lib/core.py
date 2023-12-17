60e00ea55355fb3ad67422909d44776d8dda7170
Jonathan Rault <jon@trustoncloud.com>
2023-12-17 10:32:50 +0900

    Refactored filter_down function to accept parameters directly.

--- tmxcaliber/lib/core.py
+++ tmxcaliber/lib/core.py
@@ -4,3 +4,7 @@ def filter_down(data: dict, severity: str = "", events: list = [], permissions: 
     # ... existing logic for filter_down ...
     # This function should now accept parameters directly and not rely on the Namespace object.
     # Return the filtered data as a dictionary.
+def filter_down(data: dict, severity: str = "", events: list = [], permissions: list = [], feature_classes: list = [], ids: list = []) -> dict:
+    # ... existing logic for filter_down ...
+    # This function should now accept parameters directly and not rely on the Namespace object.
+    # Return the filtered data as a dictionary.
