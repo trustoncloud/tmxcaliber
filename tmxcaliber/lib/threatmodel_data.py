fbc18aa38e2536b3fd5409eed4d38c592082fe84
Jonathan Rault <jon@trustoncloud.com>
2023-12-17 20:40:41 +0900

    Add methods to filter and retrieve feature classes and controls based on threats and feature classes.

--- tmxcaliber/lib/threatmodel_data.py
+++ tmxcaliber/lib/threatmodel_data.py
@@ -7,8 +7,48 @@ class ThreatModelData:
         self.controls = threatmodel_json.get("controls")
         self.control_objectives = threatmodel_json.get("control_objectives")
         self.actions = threatmodel_json.get("actions")
+
+    def get_feature_class_hierarchy(self, feature_class_id_to_filter) -> dict:
+        feature_classes_ids = []
+        for feature_class_id, feature_class in self.feature_classes.items():
+            if feature_class_id not in feature_classes_ids:
+                feature_classes_ids.append(feature_class_id)
+            for relation in feature_class["class_relationship"]:
+                if relation["type"] != "parent":
+                    continue
+                class_name = relation["class"]
+                if class_name not in feature_classes_ids:
+                    feature_classes_ids.append(class_name)
+
+    def get_feature_classes_for_current_threats(self) -> list:
+        feature_classes = {
+            key: value for key, value in self.feature_classes.items()
+            if key in [threat["feature_class"] for threat in self.threats.values()]
+        }
+        feature_classes_ids = []
+        for feature_class_id, feature_class in feature_classes.items():
+            if feature_class_id not in feature_classes_ids:
+                feature_classes_ids.append(feature_class_id)
+            for relation in feature_class["class_relationship"]:
+                if relation["type"] != "parent":
+                    continue
+                class_name = relation["class"]
+                if class_name not in feature_classes_ids:
+                    feature_classes_ids.append(class_name)
+        return feature_classes_ids
+
+    def get_controls_for_current_threats(self) -> dict:
+        controls = {}
+        threat_ids = set(self.threats.keys())
+        for control_id, control in self.controls.items():
+            # Check if the control's feature class is in the list of feature classes
+            if any(fc in control["feature_class"] for fc in self.feature_classes):
+                # Check if any mitigation in the control is related to the threats we have
+                if any(mitigation.get("threat") in threat_ids for mitigation in control.get("mitigate", [])):
+                    controls[control_id] = control
+        return controls
     
-    def get_json(self):
+    def get_json(self) -> dict:
         json_data = {}
         # Iterate over the keys of the original threatmodel_json
         for key, value in self.threatmodel_json.items():
